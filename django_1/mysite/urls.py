c3ef72fa627de58309da7190e01ec89084bf88c3
Kassid <lnamkyung@gmail.com>
2022-06-30 13:28:24 +0900

    modifid loginout

--- django_1/mysite/urls.py
+++ django_1/mysite/urls.py
@@ -14,15 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.contrib.auth import views
-from django.conf.urls import include, url
-
+from django.urls import include, path
 from django.contrib.auth import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^accounts/login/$', views.login, name='login'),
-    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
-    url(r'', include('blog.urls')),
+    path(r'^admin/', admin.site.urls),
+    path(r'^accounts/login/$', views.LoginView.as_view(template_name="registration/login.html"), name='login'),
+    path(r'^accounts/logout/$', views.LogoutView.as_view(template_name="registration/logout.html"), name='logout', kwargs={'next_page': '/'}),
+    path(r'', include('blog.urls')),
 ]
+
